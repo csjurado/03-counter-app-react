b7df8999f591ef57ac979521102831140bfe1584
csjurado <carlos.santiago.jurado.bazan@gmail.com>
2023-10-26 00:12:07 -0500

    second commit

---
+++ src/PrimeraApp.js
@@ -0,0 +1,7 @@
+// Functional Components
+
+const PrimeraApp =()=>{
+    return <h1> Hola mundo </h1>;
+}
+
+export default PrimeraApp;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM  from "react-dom";
+import PrimeraApp from "./PrimeraApp";
+import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const divRoot = document.querySelector('#app');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// ReactDOM.render(<PrimeraApp/>, divRoot);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// REact 18 
+const app = document.getElementById('app');
+const root = createRoot(app);
+root.render(<PrimeraApp/>);
